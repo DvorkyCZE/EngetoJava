35f367a36a0441c792c4bdd5f6c564546b5dfb7a
Marek Dvorak <marekdvorak.job@gmail.com>
2021-10-10 21:19:01 +0200

    Nahrani souboru

---
+++ DruhyProjekt/src/com/engeto/examples/Main.java
@@ -0,0 +1,22 @@
+package com.engeto.examples;
+
+public class Main {
+    /**
+     * Main method: Program starts by running this method.
+     */
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("I have passed the test!");
+
+        int delkaStranyA = 10;
+        int delkaStranyB = 5;
+
+        int plochaObdelniku;
+        plochaObdelniku = delkaStranyA * delkaStranyB;
+
+        System.out.println("Plocha obdelniku se stranami A:" +delkaStranyA+ "m a B: " +delkaStranyB+ "m je: "+plochaObdelniku+ "m^2.");
+
+    
+
+    }
+}
---
+++ HelloWorld/src/com/engeto/Main.java
@@ -0,0 +1,12 @@
+package com.engeto;
+
+public class Main {
+
+    /**
+     * Main method: program starts by running this method
+     */
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+    }
+}
